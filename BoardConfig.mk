7e168a81811d028f42e27aa5508260deccbc9b5e
snapboss <mbajrami95@gmail.com>
2025-09-07 16:14:35 +0000

    duchamp: add AudioEffects required for Dolby Atmos

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ TARGET_SCREEN_DENSITY := 440
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Kernel
