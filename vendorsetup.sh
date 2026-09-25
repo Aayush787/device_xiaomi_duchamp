a448242e276e1e3fc949cf5028dce5eaa8f3c76d
Aayush787 <crazyaayush787@gmail.com>
2026-09-25 05:28:46 +0000

    duchamp: vendorsetup: Add Aperture Cam & Bluetooth Fixups!
    Signed-off-by: Aayush787 <crazyaayush787@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+echo "Unlock 4K 60FPS Cam Recording"
+cd p*/a*/Aperture
+git fetch https://github.com/Nothing-2A/android_packages_apps_Aperture.git
+git cherry-pick 9509277efc852ad8bdcce204e0d9cfe104b6d190
+cd ../../..
+
+echo "Fixup! L2CAP and A2DP offload coex mechanism for MTK"
+cd packages/modules/Bluetooth
+git fetch https://github.com/mt6897-devs/packages_modules_Bluetooth.git
+git cherry-pick bea05a62eef1fcc37e5a10d476580a017a0bd32e
+cd ../../..
+
 echo "Provide libbase-v34"
 cd hardware/lineage/compat
 git fetch https://github.com/mt6897-devs/hardware_lineage_compat.git
