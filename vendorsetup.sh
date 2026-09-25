6b9f32ebaf03bef0cfbe31868a3aaa8a23d0ded4
Aayush787 <crazyaayush787@gmail.com>
2026-09-25 05:16:55 +0000

    duchamp: vendorsetup: Provide libbase-v34
    Signed-off-by: Aayush787 <crazyaayush787@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,8 @@
+echo "Provide libbase-v34"
+cd hardware/lineage/compat
+git fetch https://github.com/mt6897-devs/hardware_lineage_compat.git
+git cherry-pick e167e38df9e0e8623566b006dfdca0dcc908becf
+cd ../../..
+
 echo "Exporting prebuilt headers path"
 export TARGET_PREBUILT_KERNEL_HEADERS=device/xiaomi/duchamp-kernel/kernel-uapi-headers.tar.gz
